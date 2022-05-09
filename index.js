25472e7bd2e602b8454078e74ab746a57fe6f2f8
Nadeem Khan <nadeem@indorse.io>
2022-05-09 23:19:25 +0530

    cors error

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
 const authenticatedRoute = require("./routes");
 const config = require("./config");
+const app = express();
 
 const corsOpts = {
   origin: "*",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 app.use(cors(corsOpts));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const mongoURI =
-  "mongodb://nadeemkhan:B2QJZsgCOQWTWSZq@development-shard-00-00.wegse.mongodb.net:27017,development-shard-00-01.wegse.mongodb.net:27017,development-shard-00-02.wegse.mongodb.net:27017/metakeep?ssl=true&replicaSet=atlas-t39yui-shard-0&authSource=admin&retryWrites=true&w=majority";
+app.use("/api", authenticatedRoute);
 
 mongoose.connect(config.MONGO_URI, {
   useNewUrlParser: true,
@@ -25,8 +24,6 @@ mongoose.connect(config.MONGO_URI, {
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to database"));
-
-app.use("/api", authenticatedRoute);
 
 const PORT = 3001;
 
