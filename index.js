769940b5dbe4953c6a21bdc5906315fd7ae2a7e9
Nadeem Khan <nadeem@indorse.io>
2022-05-09 23:36:51 +0530

    added port var

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to database"));
 
+const PORT = process.env.PORT;
+
 app.listen(process.env.PORT || 3001, () =>
   console.log(`server running at ${PORT}`)
 );
